5016761baee84cec70841d746274e3dce16b5e4d
huangxihuan <510110003@qq.com>
2020-09-20 22:39:13 +0800

    eee

--- src/main/java/com/pumkpin/user/controller/UserController.java
+++ src/main/java/com/pumkpin/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.pumkpin.user.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @PROJECT_NAME: user
  * @DESCRIPTION:
  * @USER: huangxihuan
  * @DATE: 2020/9/19 0019 23:48
  */
-//2333
+//23
+@RestController
 public class UserController {
 }
